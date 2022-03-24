50470fde75761010c04dd67fe4aefe2e7f33cf7f
tuutti <tuutti@iki.fi>
2022-03-24 07:54:25 +0200

    UHF-3389: Proxy config

---
+++ public/sites/default/development.settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/dev-etusivu/openid-connect/tunnistamo';
+$config['helfi_proxy.settings']['asset_path'] = 'dev-etusivu-assets';
+$config['helfi_proxy.settings']['prefixes'] = [
+  'en' => 'dev-front',
+  'fi' => 'dev-etusivu',
+  'sv' => 'dev-framsidan',
+];
+
