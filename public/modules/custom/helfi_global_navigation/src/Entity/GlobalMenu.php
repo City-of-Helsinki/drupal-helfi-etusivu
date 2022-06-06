e7770724df8f27dab6ccb85eebc36f784a83ef31
Jere Ljungberg <jere@mysteerio.fi>
2022-06-06 09:15:37 +0300

    UHF-5843: Global navigation POC

---
+++ public/modules/custom/helfi_global_navigation/src/Entity/GlobalMenu.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\helfi_global_navigation\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+
+/**
+ * Defines global_menu entity class.
+ *
+ * @ContentEntityType(
+ *   id = "global_menu",
+ *   fieldable = FALSE,
+ *   label = @Translation("HELfi Global menu"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\Core\Entity\EntityViewBuilder",
+ *     "list_builder" = "Drupal\helfi_global_navigation\Entity\Listing\ListBuilder",
+ *     "form" = {
+ *       "default" = "Drupal\helfi_global_navigation\Form\GlobalMenuForm",
+ *       "delete" = "Drupal\Core\Entity\EntityDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\helfi_global_navigation\Entity\Routing\GlobalMenuRouteProvider"
+ *     }
+ *   },
+ *   base_table = "global_menu",
+ *   data_table = "global_menu_field_data",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "langcode" = "langcode"
+ *   },
+ *   translatable = TRUE,
+ *   admin_permission = "access content",
+ *   links = {
+ *     "canonical" = "/global_menu/{global_menu}",
+ *     "edit-form" = "/admin/content/integrations/global_menu/{global_menu}/edit",
+ *     "collection" = "/admin/content/integrations/global_menu",
+ *     "delete-form" = "/admin/content/integrations/global_menu/{global_menu}/delete"
+ *   }
+ * )
+ */
+class GlobalMenu extends ContentEntityBase implements ContentEntityInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['project'] = BaseFieldDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Project'))
+      ->setSetting('max_length', 50)
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'label' => 'inline',
+        'type' => 'readonly_field_widget',
+      ])
+      ->setDisplayConfigurable('form', TRUE);
+
+    $fields['name'] = BaseFieldDefinition::create('string')
+      ->setLabel(new TranslatableMarkup('Name'))
+      ->setSetting('max_length', 50)
+      ->setRequired(TRUE)
+      ->setDisplayOptions('form', [
+        'label' => 'inline',
+        'type' => 'readonly_field_widget',
+      ])
+      ->setDisplayConfigurable('form', TRUE);
+
+    $fields['menu_tree'] = BaseFieldDefinition::create('json')
+      ->setLabel(new TranslatableMarkup('Menu tree'))
+      ->setDisplayOptions('form', [
+        'type' => 'json_editor',
+      ])
+      ->setReadOnly(TRUE)
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayConfigurable('view', TRUE);
+
+    return $fields;
+  }
+
+}
