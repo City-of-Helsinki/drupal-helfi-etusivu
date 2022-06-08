8a9f068d98f606284251e996b2ec1cc7f5b274a4
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-06-08 14:13:38 +0300

    UHF-5912: update settings.php

---
+++ public/modules/custom/helfi_etusivu/src/Plugin/search_api/data_type/Image.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\helfi_etusivu\Plugin\search_api\data_type;
+
+use Drupal\image\Entity\ImageStyle;
+use Drupal\search_api\Plugin\search_api\data_type\StringDataType;
+
+/**
+ * Get file url from media entity.
+ *
+ * @SearchApiDataType(
+ *   id = "etusivu_image",
+ *   label = @Translation("Image"),
+ *   description = @Translation("Image"),
+ *   default = "true"
+ * )
+ */
+class Image extends StringDataType {
+  public function getValue($value) {
+    if (!$value->hasField('field_media_image')) {
+      return '';
+    }
+
+    if ($file = $value->get('field_media_image')->entity) {
+      try {
+        $imageStyle = ImageStyle::load('3_2_l');
+        return $imageStyle->buildUrl($file->getFileUri());
+      }
+      catch (\Exception) {
+      }
+    }
+  }
+}
