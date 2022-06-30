43ce64d6027772fd3e2036021952bcfc5ebe7dea
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2022-06-30 12:58:55 +0300

    UHF-5925: Updated paths for frontpage dev, test and staging.

--- public/sites/default/staging.settings.php
+++ public/sites/default/staging.settings.php
@@ -3,8 +3,8 @@
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/staging-etusivu/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'staging-etusivu-assets';
 $config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'staging-front',
+  'en' => 'staging-frontpage',
   'fi' => 'staging-etusivu',
-  'sv' => 'staging-framsidan',
+  'sv' => 'staging-huvudsida',
 ];
 
--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -3,8 +3,8 @@
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/test-etusivu/openid-connect/tunnistamo';
 $config['helfi_proxy.settings']['asset_path'] = 'test-etusivu-assets';
 $config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'test-front',
+  'en' => 'test-frontpage',
   'fi' => 'test-etusivu',
-  'sv' => 'test-framsidan',
+  'sv' => 'test-huvudsida',
 ];
 
