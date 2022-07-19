dc64eaa79021e467c8bfb3817f8e30556484ae2c
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-07-19 11:08:25 +0300

    UHF-6038: code fixes

--- public/modules/custom/helfi_global_navigation/src/MenuCache.php
+++ public/modules/custom/helfi_global_navigation/src/MenuCache.php
@@ -6,9 +6,18 @@ namespace Drupal\helfi_global_navigation;
 
 use Drupal\Core\State\State;
 
+/**
+ * Caching for menu request response.
+ */
 class MenuCache {
 
-  public function __construct(private State $state){
+  /**
+   * Construct.
+   *
+   * @param \Drupal\Core\State\State $state
+   *   State.
+   */
+  public function __construct(private State $state) {
   }
 
   /**
@@ -22,7 +31,7 @@ class MenuCache {
    * @return array|null
    *   Cached response
    */
-  public function getCached(string $menu_type, string $lang_code): ?array{
+  public function getCached(string $menu_type, string $lang_code): ?array {
     return $this->state->get(sprintf('%s_%s', $menu_type, $lang_code));
   }
 
@@ -35,10 +44,8 @@ class MenuCache {
    *   Language code.
    * @param mixed $data
    *   Menu response data.
-   *
-   * @return void
    */
-  public function setCache(string $menu_type, string $lang_code, mixed $data) {
+  public function setCache(string $menu_type, string $lang_code, mixed $data): void {
     $this->state->set(sprintf('%s_%s', $menu_type, $lang_code), $data);
   }
 
