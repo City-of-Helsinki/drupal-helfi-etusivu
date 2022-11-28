18a47bead42564aa54cdda38df0f30251aa94cef
Jere Ljungberg <jere@mysteerio.fi>
2022-11-28 18:22:59 +0200

    UHF-6719: Fix tests

--- public/modules/custom/helfi_news_archive/js/src/containers/FormContainer.test.js
+++ public/modules/custom/helfi_news_archive/js/src/containers/FormContainer.test.js
@@ -1,14 +1,18 @@
-import { renderWithStore } from '../test/test-utils';
+import { StateProvider } from '@appbaseio/reactivesearch';
+
+import { renderWithStore as render } from '../test/test-utils';
 import FormContainer from './FormContainer';
 
-const mockInitialState = {
+const initialParams = {
   topics: [{ label: 'Kaupunki ja hallinto', value: 'Kaupunki ja hallinto' }],
   neighbourhoods: [],
   groups: [],
 };
 
 test('Renders correctly', () => {
-  const formContainer = renderWithStore(<FormContainer initialState={mockInitialState} />);
+  const formContainer = render(
+    <StateProvider render={(props) => <FormContainer initialParams={initialParams} {...props} />} />
+  );
 
   // Should render submit button
   const submitButton = document.querySelector('button[type="submit"]');
@@ -17,24 +21,4 @@ test('Renders correctly', () => {
   // Should render 3 dropdown with labels
   const labels = document.querySelectorAll('label');
   expect(labels.length).toEqual(3);
-
-  // Should remove selections when clear all -button is pressed
-  const removeAll = document.querySelector('.news-form__clear-all button');
-  const selectionTag = document.evaluate(
-    '//span[text()="Kaupunki ja hallinto"]',
-    document,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  );
-  expect(selectionTag.singleNodeValue).not.toBe(null);
-  removeAll.click();
-  const removedSelectionTag = document.evaluate(
-    '//span[text()="Kaupunki ja hallinto"]',
-    document,
-    null,
-    XPathResult.FIRST_ORDERED_NODE_TYPE,
-    null
-  );
-  expect(removedSelectionTag.singleNodeValue).toBe(null);
 });
