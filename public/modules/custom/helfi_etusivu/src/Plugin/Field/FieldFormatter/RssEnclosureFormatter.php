fc92742a6490162539ec3c902bb09e56b3862a0f
Risto-Pekka Nykänen <risto.nykanen@druid.fi>
2022-12-19 13:32:46 +0200

    UHF-7620: new formatter to get proper image style for the rss enclosure

---
+++ public/modules/custom/helfi_etusivu/src/Plugin/Field/FieldFormatter/RssEnclosureFormatter.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\helfi_etusivu\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\file\Entity\File;
+use Drupal\image\Entity\ImageStyle;
+use Drupal\media\MediaInterface;
+
+/**
+ * Plugin implementation to get image into enclosure in rss feed.
+ *
+ * @FieldFormatter(
+ *   id = "rss_enclosure_formatter",
+ *   label = @Translation("Rss enclosure formatter"),
+ *   field_types = {
+ *     "entity_reference",
+ *   },
+ * )
+ */
+class RssEnclosureFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $elements = [];
+    foreach ($items as $delta => $item) {
+      if (
+        $item->entity instanceof MediaInterface &&
+        $item->entity->hasField('field_media_image')
+      ) {
+        $image_style = ImageStyle::load('og_image');
+        $image_entity = $item->entity->field_media_image;
+
+        if ($image_entity && $image_entity->isEmpty()) {
+          break;
+        }
+
+        $image_path = $image_entity->entity->getFileUri();
+        $image_uri = $image_style->buildUri($image_path);
+
+        // Create derivative if necessary.
+        if (!file_exists($image_uri)) {
+          $image_style->createDerivative($image_path, $image_uri);
+        }
+
+        $elements[$delta] = [
+          '#markup' => $image_style->buildUrl($image_path)
+        ];
+      }
+    }
+
+    return $elements;
+  }
+
+}
