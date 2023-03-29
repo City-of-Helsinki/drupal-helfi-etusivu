3b5dcba9e1ee710d586c847d273f78fe772903b9
mmyllynen <4696381+dire@users.noreply.github.com>
2023-03-29 08:04:30 +0300

    UHF-8141: Added prefixes for staging and testing environments.

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -6,5 +6,16 @@ $config['helfi_proxy.settings']['prefixes'] = [
   'en' => 'test-frontpage',
   'fi' => 'test-etusivu',
   'sv' => 'test-huvudsida',
+  'ar' => 'test-etusivu',
+  'de' => 'test-etusivu',
+  'es' => 'test-etusivu',
+  'et' => 'test-etusivu',
+  'fa' => 'test-etusivu',
+  'fr' => 'test-etusivu',
+  'ru' => 'test-etusivu',
+  'se' => 'test-etusivu',
+  'so' => 'test-etusivu',
+  'uk' => 'test-etusivu',
+  'zh-hans' => 'test-etusivu',
 ];
 
