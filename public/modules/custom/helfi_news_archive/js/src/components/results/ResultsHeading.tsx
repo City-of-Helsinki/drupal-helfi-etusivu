682993da224411130b87efa2e8817ed8d15b238f
Krister Haverinen <krister.haverinen@siili.com>
2023-04-27 13:30:51 +0300

    Add RSS feed with news filters

--- public/modules/custom/helfi_news_archive/js/src/components/results/ResultsHeading.tsx
+++ public/modules/custom/helfi_news_archive/js/src/components/results/ResultsHeading.tsx
@@ -1,6 +1,8 @@
 import { StateProvider } from '@appbaseio/reactivesearch';
+import { IconRss } from 'hds-react';
 
 import SearchComponents from '../../enum/SearchComponents';
+import RssFeedLink from './RssFeedLink';
 
 export const ResultsHeading = () => {
   const { RESULTS, SUBMIT } = SearchComponents;
@@ -10,12 +12,18 @@ export const ResultsHeading = () => {
       includeKeys={['value', 'hits']}
       render={({ searchState }) => (
         <h3 className='news-archive__heading'>
-          {searchState[SUBMIT] && searchState[SUBMIT].value && searchState[RESULTS] && searchState[RESULTS].hits
-            ? Drupal.t(
-                Drupal.t('News based on your choices', {}, { context: 'News archive heading' }) +
-                  ` (${searchState[RESULTS].hits ? searchState[RESULTS].hits.total : 0})`
-              )
-            : Drupal.t('All news items', {}, { context: 'News archive heading' })}
+          <span>
+            {searchState[SUBMIT] && searchState[SUBMIT].value && searchState[RESULTS] && searchState[RESULTS].hits
+              ? Drupal.t(
+                  Drupal.t('News based on your choices', {}, { context: 'News archive heading' }) +
+                    ` (${searchState[RESULTS].hits ? searchState[RESULTS].hits.total : 0})`
+                )
+              : Drupal.t('All news items', {}, { context: 'News archive heading' })}
+          </span>
+          <span className='news-archive__feed-link'>
+            <IconRss />
+            <RssFeedLink />
+          </span>
         </h3>
       )}
     />
