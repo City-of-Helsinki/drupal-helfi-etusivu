a7bc83c2deab09c9854680a9634bd36f435830f0
tuutti <tuutti@iki.fi>
2024-02-13 10:58:18 +0200

    UHF-9546: New test and staging addresses

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,21 +1,3 @@
 <?php
 
-$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/test-etusivu/openid-connect/tunnistamo';
-$config['helfi_proxy.settings']['asset_path'] = 'test-etusivu-assets';
-$config['helfi_proxy.settings']['prefixes'] = [
-  'en' => 'test-frontpage',
-  'fi' => 'test-etusivu',
-  'sv' => 'test-huvudsida',
-  'ar' => 'test-etusivu',
-  'de' => 'test-etusivu',
-  'es' => 'test-etusivu',
-  'et' => 'test-etusivu',
-  'fa' => 'test-etusivu',
-  'fr' => 'test-etusivu',
-  'ru' => 'test-etusivu',
-  'se' => 'test-etusivu',
-  'so' => 'test-etusivu',
-  'uk' => 'test-etusivu',
-  'zh-hans' => 'test-etusivu',
-];
-
+$config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
