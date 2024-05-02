00cde0fd28757b11dfbd8e9458f3ddbb59aee2a9
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2024-05-02 08:53:47 +0300

    Add helfi_annif module

---
+++ public/modules/custom/helfi_annif/src/Keyword/KeywordGeneratorException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_annif\Keyword;
+
+/**
+ * Exceptions for keyword generator errors.
+ */
+class KeywordGeneratorException extends \Exception {
+}
