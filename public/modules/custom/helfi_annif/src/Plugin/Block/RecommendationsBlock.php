705e99e8c584699de2e3197a758d98a9de286ba0
rpnykanen <risto.nykanen@druid.fi>
2024-05-27 07:30:28 +0300

    UHF-9962: get recommended news

--- public/modules/custom/helfi_annif/src/Plugin/Block/RecommendationsBlock.php
+++ public/modules/custom/helfi_annif/src/Plugin/Block/RecommendationsBlock.php
@@ -4,9 +4,21 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_annif\Plugin\Block;
 
+use Drupal\Component\Plugin\Exception\ContextException;
+use Drupal\Core\Annotation\ContextDefinition;
 use Drupal\Core\Block\Attribute\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Plugin\Context\ContextRepositoryInterface;
+use Drupal\Core\Plugin\Context\EntityContextDefinition;
+use Drupal\Core\Plugin\ContextAwarePluginAssignmentTrait;
+use Drupal\Core\Plugin\ContextAwarePluginInterface;
+use Drupal\Core\Plugin\ContextAwarePluginTrait;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\helfi_annif\RecommendationManager;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides 'AI powered recommendations'.
@@ -14,13 +26,51 @@ use Drupal\Core\StringTranslation\TranslatableMarkup;
 #[Block(
   id: "helfi_recommendations",
   admin_label: new TranslatableMarkup("AI powered recommendations"),
+  context_definitions: [
+    'node' => new \Drupal\Core\Plugin\Context\ContextDefinition('entity:node', new TranslatableMarkup('Node'), FALSE)
+  ]
 )]
-class RecommendationsBlock extends BlockBase {
+class RecommendationsBlock extends BlockBase implements ContainerFactoryPluginInterface, LoggerAwareInterface, ContextAwarePluginInterface {
+
+  use LoggerAwareTrait;
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    private readonly ContextRepositoryInterface $contextRepository,
+    private readonly RecommendationManager $recommendationManager
+  ) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) : static {
+    return new static($configuration, $plugin_id, $plugin_definition,
+      $container->get('context.repository'),
+      $container->get('helfi_annif.recommendation_manager')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() : array {
+    $x = 1;
+    try {
+      $node = $this->getContextValue('node');
+      $results = $this->recommendationManager->getRecommendations($node);
+    }
+    catch (ContextException $exception) {
+      $x = 1;
+      // $this->logger->error($exception->getMessage());
+      return [];
+    }
+
+
+
     // @todo UHF-9962.
     return [
       '#markup' => $this->t('Hello, World!'),
--- public/modules/custom/helfi_annif/src/RecommendationManager.php
+++ public/modules/custom/helfi_annif/src/RecommendationManager.php
@@ -1,13 +1,84 @@
-<?php
+<?php /** @noinspection SqlDialectInspection */
 
 declare(strict_types=1);
 
 namespace Drupal\helfi_annif;
 
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+
 /**
  * The recommendation manager.
- *
- * @todo UHF-9962.
  */
-class RecommendationManager {
+class RecommendationManager implements LoggerAwareInterface {
+
+  use LoggerAwareTrait;
+
+  /**
+   * The constructor.
+   *
+   * @param EntityTypeManagerInterface $entityManager
+   * @param Connection $connection
+   */
+  public function __construct(
+    private readonly EntityTypeManagerInterface $entityManager,
+    private readonly Connection $connection
+  ) {
+  }
+
+  /**
+   * Get recommendations for a node.
+   *
+   * @param EntityInterface $node
+   *   The node
+   *
+   * @return array
+   *   Array of recommendations
+   */
+  public function getRecommendations(EntityInterface $node): array {
+
+    $query = "
+      select
+         n.nid,
+         count(n.nid) as relevancy
+      from node as n
+      left join node__field_annif_keywords as annif on n.nid = annif.entity_id
+      where annif.field_annif_keywords_target_id in
+         (select
+          field_annif_keywords_target_id
+          from node__field_annif_keywords
+          where entity_id = :nid and
+          langcode = 'fi')
+      and n.langcode = 'fi'
+      and annif.langcode = 'fi'
+      and n.nid != :nid
+      group by n.nid
+      order by relevancy DESC
+      limit 4;
+    ";
+
+    $results = $this->connection->query($query, [':nid' => $node->id()])->fetchAll();
+
+    if (!$results || !is_array($results)) {
+      return [];
+    }
+
+    $nids = array_column($results, 'nid');
+
+    try {
+      $entities = $this->entityManager
+        ->getStorage($node->getEntityTypeId())
+        ->loadMultiple($nids);
+    }
+    catch(\Exception $e) {
+      $this->logger->error($e->getMessage());
+      return [];
+    }
+
+    return $entities;
+  }
+
 }
