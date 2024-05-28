d84d79c138688b336b275059e047dac13617ebf2
rpnykanen <risto.nykanen@druid.fi>
2024-05-28 07:30:48 +0300

    UHF-9962: phpstan error

--- public/modules/custom/helfi_annif/src/RecommendationManager.php
+++ public/modules/custom/helfi_annif/src/RecommendationManager.php
@@ -64,7 +64,7 @@ class RecommendationManager implements LoggerAwareInterface {
 
     $response = [];
     try {
-      $results = $this->connection->query($query, [':nid' => $node->id(), ':langcode' => $node->get('langcode')->value])->fetchAll();
+      $results = $this->connection->query($query, [':nid' => $node->id(), ':langcode' => $node->language()->getId()])->fetchAll();
     }
     catch (\Exception $e) {
       $this->logger->error($e->getMessage());
