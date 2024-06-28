6a868b6cef0c1f0195ec37fe76afd120b8859c91
rpnykanen <risto.nykanen@druid.fi>
2024-06-28 13:41:03 +0300

    UHF-9964: missing trait

--- public/modules/custom/helfi_etusivu/src/Entity/Node/NewsItem.php
+++ public/modules/custom/helfi_etusivu/src/Entity/Node/NewsItem.php
@@ -9,15 +9,12 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
 use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
 use Drupal\helfi_annif\RecommendableBase;
-use Drupal\helfi_annif\RecommendableEntityTrait;
 use Drupal\radioactivity\RadioactivityInterface;
 
 /**
  * A bundle class for NewsItem -node.
  */
 final class NewsItem extends RecommendableBase {
-
-  use RecommendableEntityTrait;
 
   /**
    * {@inheritdoc}
