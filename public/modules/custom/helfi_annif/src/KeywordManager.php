6f18cbbab7a43ddf8266aa5d7e627de2d8c8e0e6
rpnykanen <risto.nykanen@druid.fi>
2024-07-05 12:45:13 +0300

    UHF-9964: added keywords as node cache tags

--- public/modules/custom/helfi_annif/src/KeywordManager.php
+++ public/modules/custom/helfi_annif/src/KeywordManager.php
@@ -237,7 +237,7 @@ final class KeywordManager {
     // processedItems is set for update hooks.
     $this->processedItems[$this->getEntityKey($entity)] = TRUE;
 
-    $entity->invalidateKeywordsCacheTags();
+    // $entity->invalidateKeywordsCacheTags();
     $entity->save();
   }
 
