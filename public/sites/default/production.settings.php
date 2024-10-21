e9cebb6064ce803c3a0666a2835702f58ca696e2
rpnykanen <risto.nykanen@druid.fi>
2024-10-21 08:57:22 +0300

    UHF-10812: added for test and stage envs as well

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -3,4 +3,4 @@
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
 
 // Sitemap paging leads back to the sitemap index without this.
-$config['simple_sitemap.settings']['base_url'] = 'https://hel.fi/fi';
+$config['simple_sitemap.settings']['base_url'] = 'https://www.hel.fi/fi';
