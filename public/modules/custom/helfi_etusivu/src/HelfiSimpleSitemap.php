53c0b6d4519fb5dc00b3a5858d5b4e77650a1aaf
rpnykanen <risto.nykanen@druid.fi>
2024-10-21 15:37:52 +0300

    UHF-10812: remove the bad solution, override the original simplesitemap class and pass current language to toUrl function to get the pagination working

---
+++ public/modules/custom/helfi_etusivu/src/HelfiSimpleSitemap.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Drupal\helfi_etusivu;
+
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\simple_sitemap\Entity\SimpleSitemap;
+
+class HelfiSimpleSitemap extends SimpleSitemap {
+
+  public function __construct(
+    array $values,
+    $entity_type,
+    private LanguageManagerInterface $languageManager) {
+    parent::__construct($values, $entity_type);
+  }
+
+  public function toUrl($rel = 'canonical', array $options = []) {
+    $parameters = isset($options['delta']) ? ['page' => $options['delta']] : [];
+
+    if ($parameters['page']) {
+      //$langcode = \Drupal::languageManager()->getCurrentLanguage();
+      $language = $this->languageManager->getCurrentLanguage();
+      $options['language'] = $language;
+    }
+
+    return parent::toUrl($rel, $options);
+  }
+
+}
