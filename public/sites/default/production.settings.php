e865d27d90e72c1a152f2a3fe8e3a77886c8d2c1
rpnykanen <risto.nykanen@druid.fi>
2024-10-21 15:37:52 +0300

    UHF-10812: remove the bad solution, override the original simplesitemap class and pass current language to toUrl function to get the pagination working

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -1,6 +1,3 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
-
-// #UHF-10812 Sitemap paging leads back to the sitemap index.
-$config['simple_sitemap.settings']['base_url'] = 'https://www.hel.fi/fi';
--- public/sites/default/staging.settings.php
+++ public/sites/default/staging.settings.php
@@ -1,5 +1,3 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
-
-$config['simple_sitemap.settings']['base_url'] = 'https://www.stage.hel.ninja/fi';
