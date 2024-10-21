1c9acc9cb4e543a1f5275a85d3953d50846ab1c3
rpnykanen <risto.nykanen@druid.fi>
2024-10-21 08:57:22 +0300

    UHF-10812: added for test and stage envs as well

--- public/sites/default/testing.settings.php
+++ public/sites/default/testing.settings.php
@@ -1,3 +1,5 @@
 <?php
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
+
+$config['simple_sitemap.settings']['base_url'] = 'https://www.test.hel.ninja/fi/sitemap.xml';
