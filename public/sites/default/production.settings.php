d10fb49e6f6f83b08efca5c9a19c9bc1e85c2c2d
rpnykanen <risto.nykanen@druid.fi>
2024-10-21 09:00:43 +0300

    UHF-10812: better comment

--- public/sites/default/production.settings.php
+++ public/sites/default/production.settings.php
@@ -2,5 +2,5 @@
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
 
-// Sitemap paging leads back to the sitemap index without this.
+// #UHF-10812 Sitemap paging leads back to the sitemap index.
 $config['simple_sitemap.settings']['base_url'] = 'https://www.hel.fi/fi';
