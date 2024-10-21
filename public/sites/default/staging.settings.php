f7f78b8547b20b2470824224ba3c37c1386f28e6
rpnykanen <risto.nykanen@druid.fi>
2024-10-21 09:03:24 +0300

    UHF-10812: remove the excess from the url

--- public/sites/default/staging.settings.php
+++ public/sites/default/staging.settings.php
@@ -2,4 +2,4 @@
 
 $config['helfi_proxy.settings']['tunnistamo_return_url'] = '/fi/openid-connect/tunnistamo';
 
-$config['simple_sitemap.settings']['base_url'] = 'https://www.stage.hel.ninja/fi/sitemap.xml';
+$config['simple_sitemap.settings']['base_url'] = 'https://www.stage.hel.ninja/fi';
