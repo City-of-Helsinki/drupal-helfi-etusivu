d563163007134b4804b3ef4372d97a7c97c030ea
Jere Ljungberg <jere@mysteerio.fi>
2024-11-25 18:07:27 +0200

    UHF-10771: Move Drupal throbber to create space

--- public/modules/custom/helfi_etusivu/src/Plugin/Block/HelsinkiNearYouHeroBlock.php
+++ public/modules/custom/helfi_etusivu/src/Plugin/Block/HelsinkiNearYouHeroBlock.php
@@ -29,7 +29,7 @@ final class HelsinkiNearYouHeroBlock extends BlockBase implements ContainerFacto
     ContainerInterface $container,
     $configuration,
     $plugin_id,
-    $plugin_definition
+    $plugin_definition,
   ) {
     return new static(
       $configuration,
