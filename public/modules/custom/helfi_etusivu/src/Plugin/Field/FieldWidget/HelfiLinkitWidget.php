960894a3c70c589dce2d9bad8ff9446fae4cdd22
Jere Ljungberg <jere@mysteerio.fi>
2025-01-08 08:50:28 +0200

    UHF-9765: Remove \Drupal() call

--- public/modules/custom/helfi_etusivu/src/Plugin/Field/FieldWidget/HelfiLinkitWidget.php
+++ public/modules/custom/helfi_etusivu/src/Plugin/Field/FieldWidget/HelfiLinkitWidget.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace Drupal\helfi_etusivu\Plugin\Field\FieldWidget;
 
 use Drupal\Component\Utility\UrlHelper;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\Attribute\FieldWidget;
+use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\linkit\Plugin\Field\FieldWidget\LinkitWidget;
 use Drupal\linkit\Utility\LinkitHelper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * Overrides the linkit widget.
@@ -23,14 +28,72 @@ use Drupal\linkit\Utility\LinkitHelper;
 class HelfiLinkitWidget extends LinkitWidget {
 
   /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $plugin_id,
+      $plugin_definition,
+      $configuration['field_definition'],
+      $configuration['settings'],
+      $configuration['third_party_settings'],
+      $container->get('current_user'),
+      $container->get('entity_type.manager'),
+      $container->get('request_stack'),
+    );
+  }
+
+  /**
+   * Constructs a new instance.
+   *
+   * @param string $plugin_id
+   *   The plugin_id for the formatter.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
+   *   The definition of the field to which the formatter is associated.
+   * @param array $settings
+   *   The widget settings.
+   * @param array $third_party_settings
+   *   The widget third party settings.
+   * @param \Drupal\Core\Session\AccountProxyInterface $currentUser
+   *   The current user.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager
+   *   The entity type manager service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $requestStack
+   *   The current request stack.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  public function __construct(
+    $plugin_id,
+    $plugin_definition,
+    FieldDefinitionInterface $field_definition,
+    array $settings,
+    array $third_party_settings,
+    AccountProxyInterface $currentUser,
+    EntityTypeManagerInterface $entityTypeManager,
+    protected RequestStack $requestStack
+  ) {
+    parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $third_party_settings, $currentUser, $entityTypeManager);
+  }
+
+  /**
    * Circumvent Linkit to allow linking to internal pages directly.
    *
    * This prevents stripping language param from internal links.
+   *
+   * @param string $input
+   *   The user input.
+   *
+   * @return string
+   *   The URI.
    */
   protected function convertToUri(string $input) {
     if (
       UrlHelper::isExternal($input) &&
-      UrlHelper::externalIsLocal($input, \Drupal::request()->getSchemeAndHttpHost())
+      UrlHelper::externalIsLocal($input, $this->requestStack->getCurrentRequest()->getSchemeAndHttpHost())
     ) {
       return $input;
     }
