777d7b05f524956f59309424bab3914546d3c499
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2025-01-28 09:41:08 +0200

    UHF-10643: Hard code etusivu to suggested_topics_reference widget

--- public/modules/custom/helfi_annif/src/Plugin/Field/FieldWidget/SuggestedTopicsReferenceWidget.php
+++ public/modules/custom/helfi_annif/src/Plugin/Field/FieldWidget/SuggestedTopicsReferenceWidget.php
@@ -11,8 +11,7 @@ use Drupal\Core\Field\WidgetBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
 use Drupal\helfi_annif\Entity\SuggestedTopics;
-use Drupal\helfi_api_base\Environment\EnvironmentResolverInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\helfi_api_base\Environment\Project;
 
 /**
  * Defines the 'suggested_topics_reference' field widget.
@@ -31,26 +30,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 final class SuggestedTopicsReferenceWidget extends WidgetBase {
 
   /**
-   * The environment resolver.
-   */
-  private EnvironmentResolverInterface $environmentResolver;
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    $instance = new self(
-      $plugin_id,
-      $plugin_definition,
-      $configuration['field_definition'],
-      $configuration['settings'],
-      $configuration['third_party_settings'],
-    );
-    $instance->environmentResolver = $container->get(EnvironmentResolverInterface::class);
-    return $instance;
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state): array {
@@ -58,18 +37,11 @@ final class SuggestedTopicsReferenceWidget extends WidgetBase {
 
     $hasTargetEntity = !empty($items[$delta]->target_id) && $items[$delta]->entity;
 
-    $project = NULL;
-    try {
-      $project = $this->environmentResolver->getActiveProject()->getName();
-    }
-    catch (\Exception) {
-    }
-
     /** @var \Drupal\helfi_annif\Entity\SuggestedTopics $entity */
     $entity = $hasTargetEntity ? $items[$delta]->entity : SuggestedTopics::create([
       'parent_type' => $items->getEntity()->getEntityTypeId(),
       'parent_bundle' => $items->getEntity()->bundle(),
-      'parent_instance' => $project,
+      'parent_instance' => Project::ETUSIVU,
     ]);
 
     $element['entity'] = [
