a8863d0ce7bd0a44238f9a66b8ca8795aa9b5b28
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-04-15 09:36:27 +0300

    Reverted bb9abc4279c465afbe9188db59e439145bdbc1a7

--- public/themes/custom/hdbt_subtheme/src/js/updatingNewsTableOfContents.js
+++ public/themes/custom/hdbt_subtheme/src/js/updatingNewsTableOfContents.js
@@ -1,6 +1,4 @@
-
-
-(function(Drupal, once) {
+((Drupal, once) => {
   Drupal.behaviors.updating_news_table_of_contents = {
     attach: function attach() {
 
@@ -14,7 +12,7 @@
       const tableOfContentsList = document.querySelector('#helfi-toc-table-of-contents-list > ul');
       const mainContent = document.querySelector('main.layout-main-wrapper');
       const reservedElems = document.querySelectorAll('[id]');
-      reservedElems.forEach(function(elem) {
+      reservedElems.forEach((elem) => {
         Drupal.tableOfContents.reservedIds.push(elem.id);
       });
 
@@ -24,42 +22,42 @@
 
       // Craft table of contents for news item.
       once('updating-news-table-of-contents', titleComponents.join(','), mainContent)
-        .forEach(function(content) {
+        .forEach((content) => {
 
-          const { nodeName, anchorName } = Drupal.tableOfContents.createTableOfContentElements(content, []);
+            const { nodeName, anchorName } = Drupal.tableOfContents.createTableOfContentElements(content, []);
 
-          // On updating news there is published date under the title that we want to display in the
-          // table of contents news update version. For normal table of contents this remains empty.
-          let contentPublishDate = '';
+            // On updating news there is published date under the title that we want to display in the
+            // table of contents news update version. For normal table of contents this remains empty.
+            let contentPublishDate = '';
 
-          if (tableOfContentsNewsUpdates && content.nextSibling && content.nextElementSibling.nodeName === 'TIME') {
-            const contentPublishDateStamp = new Date(content.nextElementSibling.dateTime);
-            contentPublishDate = `${contentPublishDateStamp.getDate()}.${contentPublishDateStamp.getMonth() + 1}.${contentPublishDateStamp.getFullYear()}`;
-          }
-
-          // Create table of contents if component is enabled.
-          if (tableOfContentsList && nodeName === 'h2') {
-            const listItem = document.createElement('li');
-            listItem.classList.add('table-of-contents__item');
-
-            // Add content publish date and its wrapper to list items only if they exist.
-            if (contentPublishDate) {
-              const publishDate = document.createElement('time');
-              publishDate.dateTime = content.nextElementSibling.dateTime;
-              publishDate.textContent = contentPublishDate;
-              listItem.appendChild(publishDate);
+            if (tableOfContentsNewsUpdates && content.nextSibling && content.nextElementSibling.nodeName === 'TIME') {
+              const contentPublishDateStamp = new Date(content.nextElementSibling.dateTime);
+              contentPublishDate = `${contentPublishDateStamp.getDate()}.${contentPublishDateStamp.getMonth() + 1}.${contentPublishDateStamp.getFullYear()}`;
             }
 
-            const link = document.createElement('a');
-            link.classList.add('table-of-contents__link');
-            link.href = `#${anchorName}`;
-            link.textContent = content.textContent.trim();
+            // Create table of contents if component is enabled.
+            if (tableOfContentsList && nodeName === 'h2') {
+              const listItem = document.createElement('li');
+              listItem.classList.add('table-of-contents__item');
 
-            listItem.appendChild(link);
-            tableOfContentsList.appendChild(listItem);
+              // Add content publish date and its wrapper to list items only if they exist.
+              if (contentPublishDate) {
+                const publishDate = document.createElement('time');
+                publishDate.dateTime = content.nextElementSibling.dateTime;
+                publishDate.textContent = contentPublishDate;
+                listItem.appendChild(publishDate);
+              }
+
+              const link = document.createElement('a');
+              link.classList.add('table-of-contents__link');
+              link.href = `#${anchorName}`;
+              link.textContent = content.textContent.trim();
+
+              listItem.appendChild(link);
+              tableOfContentsList.appendChild(listItem);
+            }
           }
-        }
-      );
+        );
 
       if (tableOfContentsNewsUpdates) {
         Drupal.tableOfContents.updateTOC(tableOfContentsNewsUpdates);
