228b82b737d3befb52b15394105b741c2c8589c1
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2025-06-25 14:28:23 +0300

    Use the bundle class from platform config.

--- public/modules/custom/helfi_etusivu/src/Entity/Node/Announcement.php
+++ public/modules/custom/helfi_etusivu/src/Entity/Node/Announcement.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace Drupal\helfi_etusivu\Entity\Node;
 
 use Drupal\helfi_etusivu_entities\Plugin\ExternalEntities\StorageClient\Announcements;
-use Drupal\node\Entity\Node;
+use Drupal\helfi_node_announcement\Entity\Announcement as AnnouncementNodeEntity;
 
 /**
  * A bundle class for Announcement-node.
  */
-final class Announcement extends Node implements PublishExternallyInterface {
+final class Announcement extends AnnouncementNodeEntity implements PublishExternallyInterface {
 
   use PublishExternallyTrait;
 
