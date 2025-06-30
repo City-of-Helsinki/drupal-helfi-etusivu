5b5b4269f7b6b241ba670643d44de8211e0d0629
Jere Ljungberg <jere@mysteerio.fi>
2025-06-30 15:43:57 +0300

    UHF-9828: Add query modifiers for elastic news rss feed

--- public/modules/custom/helfi_etusivu/src/EventSubscriber/ElasticsearchEventSubscriber.php
+++ public/modules/custom/helfi_etusivu/src/EventSubscriber/ElasticsearchEventSubscriber.php
@@ -6,6 +6,7 @@ namespace Drupal\helfi_etusivu\EventSubscriber;
 
 use Drupal\Component\Utility\NestedArray;
 use Drupal\elasticsearch_connector\Event\AlterSettingsEvent;
+use Drupal\elasticsearch_connector\Event\QueryParamsEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -19,6 +20,7 @@ class ElasticsearchEventSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       AlterSettingsEvent::class => 'prepareIndices',
+      QueryParamsEvent::class => 'prepareQueryParams',
     ];
   }
 
@@ -49,4 +51,35 @@ class ElasticsearchEventSubscriber implements EventSubscriberInterface {
     }
   }
 
+  /**
+   * Modify query to match the one we use in react app.
+   * 
+   * @param \Drupal\elasticsearch_connector\Event\QueryParamsEvent $event
+   *   The QueryParams event.
+   */
+  public function prepareQueryParams(QueryParamsEvent $event): void {
+    [$index, $body] = array_values($event->getParams());
+
+    if ($index !== 'news' || empty($body['query']['bool']['must']['query_string'])) {
+      return;
+    }
+
+    $must = $body['query']['bool']['must'];
+    $query = $must['query_string']['query'];
+    $keyword = str_split($query, strlen($query) - 1)[0];
+
+    $body['query']['bool']['should'] = [$must];
+    unset($body['query']['bool']['must']);
+    $body['query']['bool']['minimum_should_match'] = 1;
+    $body['query']['bool']['should'][] = [
+      'wildcard' => [
+        'title.keyword' => '*' . $keyword . '*',
+      ],
+    ];
+
+    $event->setParams([
+      'index' => $index,
+      'body' => $body,
+    ]);
+  }
 }
