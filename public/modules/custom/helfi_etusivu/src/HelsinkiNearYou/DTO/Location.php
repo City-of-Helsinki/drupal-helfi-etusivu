41b8734db8c571e74ecebaca4e0349ef3bf47de8
tuutti <tuutti@iki.fi>
2025-08-13 11:07:20 +0300

    UHF-12071: Initial somewhat working poc

--- public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/DTO/Location.php
+++ public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/DTO/Location.php
@@ -13,13 +13,26 @@ final readonly class Location {
   ) {
   }
 
+  /**
+   * A magic method to convert object to a string.
+   *
+   * Objects passed to lazy loader callback must be string-able in order
+   * for them to work.
+   *
+   * @return string
+   *   The location string.
+   */
+  public function __toString() : string {
+    return $this->lat . ', ' . $this->lon;
+  }
+
   public static function createFromArray(array $data) : self {
     if (!isset($data['coordinates'], $data['type'])) {
       throw new \InvalidArgumentException('Missing "coordinates" or "type".');
     }
     [$lon, $lat] = $data['coordinates'];
 
-    return new self($data['type'], (string) $lat, (string) $lon);
+    return new self((string) $lat, (string) $lon, $data['type']);
   }
 
 }
