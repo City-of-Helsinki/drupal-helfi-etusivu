4e3ba76b15d1d4e05c235c9383ccfb8e75dcac52
Tero Elonen <tero.elonen@druid.fi>
2025-08-20 11:37:40 +0300

    UHF-12055: Change the HelsinkiNearYouTextProvider functions back to return empty string as default because of PHPCS recommendation about passing empty strings to t()

--- public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/HelsinkiNearYouTextProvider.php
+++ public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/HelsinkiNearYouTextProvider.php
@@ -22,16 +22,16 @@ final class HelsinkiNearYouTextProvider {
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match object.
    *
-   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
-   *   The translated title if the route matches or an empty markup object if not.
+   * @return string|\Drupal\Core\StringTranslation\TranslatableMarkup
+   *   The translated title if the route matches or an empty string if not.
    */
-  public function getTitle(RouteMatchInterface $route_match) : TranslatableMarkup {
+  public function getTitle(RouteMatchInterface $route_match) : string|TranslatableMarkup {
     return match($route_match->getRouteName()) {
       'helfi_etusivu.helsinki_near_you' => $this->t('Helsinki near you', [], ['context' => 'Helsinki near you']),
       'helfi_etusivu.helsinki_near_you_feedbacks' => $this->t('Feedback near you', [], ['context' => 'Helsinki near you']),
       'helfi_etusivu.helsinki_near_you_events' => $this->t('Events near you', [], ['context' => 'Helsinki near you']),
       'helfi_etusivu.helsinki_near_you_roadworks' => $this->t('Street and park projects near you', [], ['context' => 'Helsinki near you']),
-      default => $this->t(''),
+      default => '',
     };
   }
 
@@ -41,18 +41,18 @@ final class HelsinkiNearYouTextProvider {
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
    *   The current route match object.
    *
-   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   * @return string|\Drupal\Core\StringTranslation\TranslatableMarkup
    *   The translated description if the route matches
-   *   or an empty markup object if not.
+   *   or an empty string if not.
    */
-  public function getDescription(RouteMatchInterface $route_match) : TranslatableMarkup {
+  public function getDescription(RouteMatchInterface $route_match) : string|TranslatableMarkup {
     return match($route_match->getRouteName()) {
       'helfi_etusivu.helsinki_near_you' => $this->t('Discover city services, events and news near you. Start by entering your street address.', [], ['context' => 'Helsinki near you']),
       'helfi_etusivu.helsinki_near_you_results' => $this->t('Discover city services, events and news near you.', [], ['context' => 'Helsinki near you']),
       'helfi_etusivu.helsinki_near_you_feedbacks' => $this->t('Find feedback in your neighbourhood.', [], ['context' => 'Helsinki near you']),
       'helfi_etusivu.helsinki_near_you_events' => $this->t('Find events in your neighbourhood that interest you.', [], ['context' => 'Helsinki near you events search']),
       'helfi_etusivu.helsinki_near_you_roadworks' => $this->t('Find street and park projects in your neighbourhood.', [], ['context' => 'Helsinki near you roadworks search']),
-      default => $this->t(''),
+      default => '',
     };
   }
 
