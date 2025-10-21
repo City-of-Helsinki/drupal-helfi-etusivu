c4709902e33c07e222c00f8583ab58d3741544c9
Risto Nykänen <risto.nykanen@druid.fi>
2025-10-21 10:54:59 +0300

    UHF-12316: dtt test for the radioactivity functionality

---
+++ public/modules/custom/helfi_etusivu/tests/src/ExistingSite/RadioactivityTest.php
@@ -0,0 +1,49 @@
+<?php
+
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_etusivu\ExistingSite;
+
+use Drupal\helfi_etusivu\Drush\Commands\RadioactivityCommand;
+use Drupal\radioactivity\RadioactivityProcessorInterface;
+use Drupal\Tests\helfi_api_base\Functional\ExistingSiteTestBase;
+
+/**
+ * Tests radioactivity customization.
+ *
+ * @group helfi_etusivu
+ */
+class RadioactivityTest extends ExistingSiteTestBase {
+
+  /**
+   * Tests custom radioactivity cron implementation.
+   *
+   * Radioactivity module's cron hook should be disabled and
+   * custom command should execute the original functionality.
+   */
+  public function testCustomRadioactivityCron(): void {
+    $service = \Drupal::service(RadioactivityCommand::class);
+    /** @var \Drupal\helfi_etusivu\Drush\Commands\RadioactivityCommand $service */
+    $previousRun = \Drupal::state()->get(RadioactivityProcessorInterface::LAST_PROCESSED_STATE_KEY);
+
+    /** @var \Drupal\Core\Cron $cron */
+    $cron = \Drupal::service('cron');
+    $cron->run();
+
+    $this->assertEquals(
+      $previousRun,
+      \Drupal::state()->get(RadioactivityProcessorInterface::LAST_PROCESSED_STATE_KEY),
+      'Assert that cron did not execute radioactivity'
+    );
+
+    $service->radioactivity();
+
+    $this->assertNotEquals(
+      $previousRun,
+      \Drupal::state()->get(RadioactivityProcessorInterface::LAST_PROCESSED_STATE_KEY),
+      'Assert that custom command executed radioactivity'
+    );
+  }
+
+}
