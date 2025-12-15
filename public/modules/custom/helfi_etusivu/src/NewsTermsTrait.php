223d006955304a056c2ee9a48bef305bde75ec35
Tero Elonen <tero.elonen@druid.fi>
2025-12-15 15:46:27 +0200

    UHF-12450: Add new shared function between the news item and article that makes the taxonomy term tags on news and article pages into links

--- public/modules/custom/helfi_etusivu/src/NewsTermsTrait.php
+++ public/modules/custom/helfi_etusivu/src/NewsTermsTrait.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_etusivu;
 
+use Drupal\Core\Entity\TranslatableInterface;
+use Drupal\Core\Field\EntityReferenceFieldItemListInterface;
+
 /**
  * Retrieve news terms from entity.
  */
@@ -35,6 +38,64 @@ trait NewsTermsTrait {
     }
 
     return $list;
+  }
+
+  /**
+   * Gets taxonomy terms with their archive links for the specified fields.
+   *
+   * @return array
+   *   An array of taxonomy terms with their archive links, keyed by field name.
+   */
+  public function getTaxonomyTermsWithArchiveLinks(): array {
+    // Map the term fields on node to term groups.
+    $fields = [
+      'field_news_item_tags' => 'topic',
+      'field_news_neighbourhoods' => 'neighbourhoods',
+      'field_news_groups' => 'groups',
+    ];
+
+    $result = [];
+
+    // Go through each field.
+    foreach ($fields as $field_name => $url_param_name) {
+      if (!$this->hasField($field_name) || $this->get($field_name)->isEmpty()) {
+        continue;
+      }
+      // Get field contents to a variable.
+      $field = $this->get($field_name);
+
+      // Make sure that the field is term reference field.
+      if ($field instanceof EntityReferenceFieldItemListInterface) {
+        // Get the referenced term entities to a variable.
+        $terms = $field->referencedEntities();
+        // Get node's language code.
+        $language = $this->language()->getId();
+        // Define language-specific base paths for news archive
+        $base_paths = [
+          'fi' => '/fi/uutiset/etsi-uutisia',
+          'sv' => '/sv/nyheter/sok-efter-nyheter',
+          'en' => '/en/news/search-for-news',
+        ];
+        // Default to English if language is not found.
+        $base_path = $base_paths[$language] ?? $base_paths['en'];
+
+        // Loop through each term
+        foreach ($terms as $term) {
+          // Get the term label in the node's language.
+          $term_name = ($term instanceof TranslatableInterface && $term->hasTranslation($language))
+            ? $term->getTranslation($language)->label()
+            : $term->label();
+
+          // Build the url and add all the information to the result array.
+          $result[$field_name][] = [
+            'name' => $term_name,
+            'url' => $base_path . '?' . $url_param_name . '[0]=' . $term->id(),
+          ];
+        }
+      }
+    }
+
+    return $result;
   }
 
 }
