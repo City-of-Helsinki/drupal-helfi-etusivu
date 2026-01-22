f604dc550406df39cbb854bc919f9e377fa80845
tuutti <tuutti@iki.fi>
2026-01-22 11:50:28 +0200

    UHF-12763: Refactored Helsinki near you blocks to use HTMX

--- public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/Controller/FeedbackController.php
+++ public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/Controller/FeedbackController.php
@@ -4,15 +4,28 @@ declare(strict_types=1);
 
 namespace Drupal\helfi_etusivu\HelsinkiNearYou\Controller;
 
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
 use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\Core\Template\Attribute;
 use Drupal\helfi_api_base\ServiceMap\DTO\Address;
+use Drupal\helfi_api_base\ServiceMap\ServiceMapInterface;
+use Drupal\helfi_etusivu\HelsinkiNearYou\Feedback\LazyBuilder;
 use Drupal\helfi_etusivu\HelsinkiNearYou\Form\FeedbackSearchForm;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * A controller to list feedback for given coordinates.
  */
 final class FeedbackController extends SearchPageControllerBase {
+
+  public function __construct(
+    private LazyBuilder $lazyBuilder,
+    ServiceMapInterface $serviceMap,
+    FormBuilderInterface $formBuilder,
+    LanguageManagerInterface $languageManager,
+  ) {
+    parent::__construct($serviceMap, $formBuilder, $languageManager);
+  }
 
   /**
    * {@inheritdoc}
@@ -38,13 +51,16 @@ final class FeedbackController extends SearchPageControllerBase {
   /**
    * {@inheritdoc}
    */
-  protected function build(Address $address): array {
-    $langcode = $this->languageManager
-      ->getCurrentLanguage()
-      ->getId();
+  public function buildHtmxResults(Address $address, string $langcode, ?int $limit = NULL): array {
+    return $this->lazyBuilder->build($address, $langcode, $limit);
+  }
 
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildFormResults(Address $address, string $langcode, Request $request): array {
     return [
-      '#content' => $this->buildFeedback($address, $langcode, NULL, new Attribute(['title_level' => ['h4']])),
+      '#content' => $this->buildFeedbackHtmxContainer($request),
       '#content_attributes' => ['classes' => ['components--helsinki-near-you-feedback-page']],
     ];
   }
