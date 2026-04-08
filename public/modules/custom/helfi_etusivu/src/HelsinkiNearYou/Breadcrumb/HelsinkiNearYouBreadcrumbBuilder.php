10c2be86321d658d3dbd07b5b52214781a54e362
Santeri Hurnanen <santeri.hurnanen@fame.fi>
2026-04-08 09:43:32 +0300

    UHF-12423: Improve breadcrump on helsinki near you page

---
+++ public/modules/custom/helfi_etusivu/src/HelsinkiNearYou/Breadcrumb/HelsinkiNearYouBreadcrumbBuilder.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\helfi_etusivu\HelsinkiNearYou\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\BreadcrumbBuilderInterface;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslatableMarkup;
+use Drupal\Core\Url;
+use Drupal\helfi_etusivu\HelsinkiNearYou\Enum\RouteInformationEnum;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Breadcrumb builder for Helsinki Near You pages.
+ */
+final class HelsinkiNearYouBreadcrumbBuilder implements BreadcrumbBuilderInterface {
+
+  use StringTranslationTrait;
+
+  public function __construct(
+    private readonly RequestStack $requestStack,
+  ) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  #[\Override]
+  public function applies(RouteMatchInterface $route_match, ?CacheableMetadata $cacheable_metadata = NULL): bool {
+    $cacheable_metadata?->addCacheContexts(['route']);
+    return RouteInformationEnum::fromRoute($route_match->getRouteName()) !== NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  #[\Override]
+  public function build(RouteMatchInterface $route_match): Breadcrumb {
+    $breadcrumb = new Breadcrumb();
+    $breadcrumb->addCacheContexts(['route', 'url.query_args:home_address']);
+
+    // Frontpage is injected as first link by
+    // helfi_platform_config_system_breadcrumb_alter.
+    $routeEnum = RouteInformationEnum::fromRoute($route_match->getRouteName());
+
+    if ($routeEnum === RouteInformationEnum::LandingPage) {
+      $breadcrumb->addLink(Link::createFromRoute(
+        RouteInformationEnum::LandingPage->getTitle([]),
+        '<none>',
+      ));
+
+      return $breadcrumb;
+    }
+
+    $breadcrumb->addLink(Link::createFromRoute(
+      RouteInformationEnum::LandingPage->getTitle([]),
+      'helfi_etusivu.helsinki_near_you',
+    ));
+
+    $address = $this->requestStack->getCurrentRequest()?->query->get('home_address');
+
+    if ($address) {
+      $text = new TranslatableMarkup("Results for @address", ['@address' => urldecode($address)], [
+        'context' => 'Helsinki near you',
+      ]);
+
+      if ($routeEnum === RouteInformationEnum::Results) {
+        // On results page, show address as current page.
+        $breadcrumb->addLink(Link::createFromRoute($text, '<none>'));
+      }
+      else {
+        // On sub-pages, link back to results with address.
+        $breadcrumb->addLink(Link::fromTextAndUrl(
+          $text,
+          Url::fromRoute('helfi_etusivu.helsinki_near_you_results', [], [
+            'query' => ['home_address' => $address],
+          ]),
+        ));
+        $breadcrumb->addLink(Link::createFromRoute($routeEnum->getTitle([]), '<none>'));
+      }
+    }
+
+    return $breadcrumb;
+  }
+
+}
---
+++ public/modules/custom/helfi_etusivu/tests/src/Kernel/HelsinkiNearYou/Breadcrumb/HelsinkiNearYouBreadcrumbBuilderTest.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\Tests\helfi_etusivu\Kernel\HelsinkiNearYou\Breadcrumb;
+
+use Drupal\Core\Breadcrumb\Breadcrumb;
+use Drupal\Core\Breadcrumb\ChainBreadcrumbBuilderInterface;
+use Drupal\Core\Routing\RouteMatch;
+use Drupal\helfi_api_base\Environment\EnvironmentEnum;
+use Drupal\helfi_api_base\Environment\Project;
+use Drupal\Tests\helfi_api_base\Traits\EnvironmentResolverTrait;
+use Drupal\KernelTests\KernelTestBase;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\RunTestsInSeparateProcesses;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Tests the Helsinki Near You breadcrumb builder.
+ */
+#[Group("helfi_etusivu")]
+#[RunTestsInSeparateProcesses]
+class HelsinkiNearYouBreadcrumbBuilderTest extends KernelTestBase {
+
+  use EnvironmentResolverTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'helfi_api_base',
+    'helfi_platform_config',
+    'helfi_etusivu',
+    'config_rewrite',
+    'system',
+    'path_alias',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    $this->setActiveProject(Project::ETUSIVU, EnvironmentEnum::Local);
+  }
+
+  /**
+   * Tests breadcrumb on sub-pages (events, roadworks, feedback) with address.
+   */
+  #[DataProvider('pagesProvider')]
+  public function testSubPageWithAddress(string $routeName, string $path, int $expectedLinkCount): void {
+    $breadcrumb = $this->buildBreadcrumb($routeName, $path, 'Kalevankatu 2');
+
+    $links = $breadcrumb->getLinks();
+    $this->assertCount($expectedLinkCount, $links);
+
+    // This link is injected by helfi_platform_config_system_breadcrumb_alter.
+    $this->assertEquals('Front page', (string) $links[0]->getText());
+    $this->assertEquals('<front>', $links[0]->getUrl()->getRouteName());
+
+    $this->assertEquals('Helsinki near you', (string) $links[1]->getText());
+
+    if ($expectedLinkCount > 2) {
+      $this->assertEquals('helfi_etusivu.helsinki_near_you', $links[1]->getUrl()->getRouteName());
+
+      // Address link should point to results page with query parameter.
+      $this->assertStringContainsString('Kalevankatu 2', (string) $links[2]->getText());
+    }
+
+    if ($expectedLinkCount > 3) {
+      $this->assertEquals('helfi_etusivu.helsinki_near_you_results', $links[2]->getUrl()->getRouteName());
+      $this->assertEquals('Kalevankatu 2', $links[2]->getUrl()->getOption('query')['home_address']);
+
+      $this->assertNotEmpty((string) $links[3]->getText());
+    }
+
+    // Last link should always be the current page.
+    $this->assertEquals('<none>', array_last($links)->getUrl()->getRouteName());
+
+    // All previous links should have a real route.
+    foreach (array_slice($links, 0, -1) as $link) {
+      $this->assertNotEquals('<none>', $link->getUrl()->getRouteName());
+    }
+  }
+
+  /**
+   * Data provider for all pages.
+   */
+  public static function pagesProvider(): array {
+    return [
+      'landing page' => [
+        'helfi_etusivu.helsinki_near_you',
+        '/helsinki-near-you',
+        2,
+      ],
+      'results' => [
+        'helfi_etusivu.helsinki_near_you_results',
+        '/helsinki-near-you/results',
+        3,
+      ],
+      'events' => [
+        'helfi_etusivu.helsinki_near_you_events',
+        '/helsinki-near-you/events',
+        4,
+      ],
+      'roadworks' => [
+        'helfi_etusivu.helsinki_near_you_roadworks',
+        '/helsinki-near-you/roadworks',
+        4,
+      ],
+      'feedback' => [
+        'helfi_etusivu.helsinki_near_you_feedback',
+        '/helsinki-near-you/feedback',
+        4,
+      ],
+    ];
+  }
+
+  /**
+   * Builds a breadcrumb for the given route.
+   */
+  private function buildBreadcrumb(string $routeName, string $path, ?string $address = NULL): Breadcrumb {
+    $query = $address ? ['home_address' => $address] : [];
+    $request = Request::create($path, 'GET', $query);
+
+    $route = new Route($path);
+    $routeMatch = new RouteMatch($routeName, $route);
+
+    $requestStack = $this->container->get('request_stack');
+    $requestStack->push($request);
+
+    try {
+      return $this->container->get(ChainBreadcrumbBuilderInterface::class)
+        ->build($routeMatch);
+    }
+    finally {
+      $requestStack->pop();
+    }
+  }
+
+}
