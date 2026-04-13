ab9f743eac0c355d1772aca3759e4a6b946f63ca
Tero Elonen <tero.elonen@druid.fi>
2026-04-13 16:57:35 +0300

    UHF-13002: Add metadata and other placeholder data for the search layout

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/site-search/components/ResultCard.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/site-search/components/ResultCard.tsx
@@ -1,19 +1,61 @@
 import CardItem from '@/react/common/Card';
+import Icon from '@/react/common/Icon';
 
 type ResultCardProps = {
   url: string;
   title: string;
   description?: string;
+  bundle?: string;
+  publishDate?: string;
 };
 
-const ResultCard = ({ url, title, description }: ResultCardProps) => (
-  <CardItem
-    cardTitle={title}
-    cardUrl={url}
-    cardDescription={description}
-    cardModifierClass={'card--site-search'}
-    cardTitleLevel={3}
-  />
-);
+const DESCRIPTION_PLACEHOLDER =
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const DATE_PLACEHOLDER = '1.1.2000';
+const PUBLISH_DATE_PLACEHOLDER = '2000-01-01';
+
+const isOlderThanOneYear = (isoDate: string): boolean => {
+  const oneYearAgo = new Date();
+  oneYearAgo.setFullYear(oneYearAgo.getFullYear() - 1);
+  return new Date(isoDate) < oneYearAgo;
+};
+
+const ResultCard = ({
+  url,
+  title,
+  description = DESCRIPTION_PLACEHOLDER,
+  bundle,
+  publishDate = PUBLISH_DATE_PLACEHOLDER,
+}: ResultCardProps) => {
+  const isNewsItem = bundle === 'news_item';
+  const isOutdated = isNewsItem && isOlderThanOneYear(publishDate);
+
+  return (
+    <CardItem
+      cardTitle={title}
+      cardUrl={url}
+      cardDescription={description}
+      cardModifierClass={'card--site-search'}
+      cardTitleLevel={3}
+      {...(isNewsItem && {
+        date: DATE_PLACEHOLDER,
+        dateLabel: Drupal.t('Published', {}, { context: 'Site search' }),
+      })}
+      {...(isOutdated && {
+        cardTags: [
+          {
+            tag: Drupal.t(
+              'Published over a year ago',
+              {},
+              { context: 'The helper text before the node published timestamp' },
+            ),
+            color: 'alert',
+            iconStart: <Icon icon='alert-circle' />,
+          },
+        ],
+      })}
+    />
+  );
+};
 
 export default ResultCard;
