a6caf7f52f7fe853a5ee39fa8855f6f49a4444a6
Markus Kalijärvi <markus.kalijarvi@druid.fi>
2026-05-29 14:50:25 +0300

    Use the helfi_users LocalTasksBlock as a parent.

--- public/modules/custom/helfi_etusivu/src/Plugin/Block/EtusivuLocalTasksBlock.php
+++ public/modules/custom/helfi_etusivu/src/Plugin/Block/EtusivuLocalTasksBlock.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 namespace Drupal\helfi_etusivu\Plugin\Block;
 
 use Drupal\Core\Language\LanguageManagerInterface;
-use Drupal\Core\Menu\Plugin\Block\LocalTasksBlock;
 use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\helfi_users\Plugin\Block\LocalTasksBlock;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
