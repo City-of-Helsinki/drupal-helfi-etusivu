56fddb7362a862400bc3838f69f45c609e4a041d
Tero Elonen <tero.elonen@druid.fi>
2026-09-11 18:23:28 +0300

    UHF-13274: Change the search results title to h2

--- public/themes/custom/hdbt_subtheme/src/js/react/apps/site-search/containers/ResultsContainer.tsx
+++ public/themes/custom/hdbt_subtheme/src/js/react/apps/site-search/containers/ResultsContainer.tsx
@@ -64,6 +64,7 @@ const ResultsContainer = ({ bundle }: ResultsContainerProps) => {
     return (
       <div key='ghost' className={resultsClassName}>
         <ResultsHeader
+          headingLevel={2}
           resultText={Drupal.t('Searching for results...', {}, { context: 'Site search' })}
           ref={loadingHeaderRef}
         />
@@ -131,6 +132,7 @@ const ResultsContainer = ({ bundle }: ResultsContainerProps) => {
     <div key='results' className={resultsClassName}>
       {DEBUG_MODE && data?.debug && <DebugBlock label='DEBUG — backend response' data={data.debug} />}
       <ResultsHeader
+        headingLevel={2}
         resultText={Drupal.formatPlural(totalHits, '@count result', '@count results', {}, { context: 'Site search' })}
         ref={scrollTarget}
       />
